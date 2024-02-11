30d89ee38b679d9e35046251b834f3f196e3c848
Mikael Kristensen <mikael@hvitfjell.no>
2024-02-11 18:31:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, get } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, get, set, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://endorsements-6064e-default-rtdb.europe-west1.firebasedatabase.app/"
