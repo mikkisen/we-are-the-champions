959ae5bb04fac3d49884a5eb6a6aee861f261b3a
Mikael Kristensen <mikael@hvitfjell.no>
2024-02-11 18:28:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, get } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://endorsements-6064e-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -51,11 +51,12 @@ publishBtn.addEventListener("click", function () {
 })
 
 endorsements.addEventListener("dblclick", function () {
-    if (!localStorage.getItem(`${currentIndex}`)) {
-        localStorage.setItem(currentIndex, +1)
-        let endorsementID = endorsementArray[currentIndex][0]
+    let endorsementID = endorsementArray[currentIndex][0]
+    if (!localStorage.getItem(`${endorsementID}`)) {
+        localStorage.setItem(endorsementID, +1)
         incrementValueAtPath(endorsementID)
         renderEndorsement()
+
     } else {
         console.log("Already added like to item")
     }
